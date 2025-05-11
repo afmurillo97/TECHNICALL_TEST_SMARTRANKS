91ac1767327833338a71cd63489b29416bcd88f6
Andres Murillo <afmurillo97@gmail.com>
2025-05-10 20:39:40 -0500

    Add CategoryCollection and ProductCollection resource classes for structured API responses
    This commit introduces two new resource collection classes, CategoryCollection and ProductCollection, which transform their respective resource collections into structured arrays for API responses. Each class includes metadata such as organization details and author information, enhancing the response format for better clarity and consistency.

---
+++ app/Http/Resources/Api/V1/ProductCollection.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources\Api\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => $this->collection,
+            'meta' =>[
+                'organization' => 'afmurillo97 Company',
+                'final_tester' => 'SMART RANKS TECHNICAL TEST',
+                'authors' => [
+                    'Felipe Murillo',
+                    'afmurillo97@gmail.com',
+                    'afmurillo97'
+                ]
+            ]
+        ];
+    }
+}
