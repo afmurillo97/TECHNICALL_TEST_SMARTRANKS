685c40b08878985e59ece1e4b318e5e78e438aaf
Andres Murillo <afmurillo97@gmail.com>
2025-05-11 02:00:01 -0500

    Add CategoryFilter class for filtering categories in API requests
    This commit introduces a new CategoryFilter class that extends the ApiFilter class, enabling dynamic filtering of categories based on safe parameters such as category name, description excerpt, and creation date. The class includes mappings for database columns and operators, enhancing the flexibility of API queries related to categories.

---
+++ app/Filters/CategoryFilter.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Filters;
+
+use Illuminate\Http\Request;
+use App\Filters\ApiFilter;
+
+class CategoryFilter extends ApiFilter {
+
+    protected $safeParams = [
+        'category_name' => ['eq', 'like'],
+        'description_excerpt' => ['eq', 'like'],
+        'url_image' => ['eq'],
+        'created_at' => ['eq', 'lt', 'lte', 'gt', 'gte'],
+    ];
+    protected $columnMap = [
+        'category_name' => 'name',
+        'description_excerpt' => 'description',
+        'url_image' => 'featured_image',
+        'created_at' => 'created_at'
+    ];
+    protected $operatorMap = [
+        'eq' => '=',
+        'like' => 'LIKE',
+        'lt' => '<',
+        'lte' => '<=',
+        'gt' => '>',
+        'gte' => '>='
+    ];
+
+}
