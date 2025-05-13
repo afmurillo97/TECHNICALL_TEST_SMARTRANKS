e615fd5777b51c5ee8ea62778e14383b9dfacca6
Andres Murillo <afmurillo97@gmail.com>
2025-05-12 20:51:18 -0500

    Add new request classes for category management
    This commit introduces three new request classes: BulkStoreCategoryRequest, StoreCategoryRequest, and UpdateCategoryRequest. These classes define validation rules and input preparation methods for handling category data during creation and updates, enhancing the overall category management functionality in the application.

---
+++ app/Http/Requests/StoreCategoryRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+
+class StoreCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string', 'max:500'],
+            'featured_image' => ['nullable', 'string'],
+            'status' => ['required', 'boolean'],
+        ];
+    }
+
+    /**
+     * This method is used to modify the input data before it is validated.
+     * 
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        $this->merge([
+            'name' => Str::headline($this->input('name')),
+            'description' => $this->filled('description') 
+            ? Str::of($this->input('description'))->trim()->ucfirst()->toString()
+            : null,
+            'status' => (bool) $this->input('status'),
+        ]);
+    }
+}
---
+++ app/Http/Requests/UpdateCategoryRequest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Str;
+
+class UpdateCategoryRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        $method = $this->method();
+        
+        if ($method == 'PUT') {
+            return [
+                'name' => ['required', 'string', 'max:255'],
+                'description' => ['nullable', 'string', 'max:500'],
+                'featured_image' => ['nullable', 'string'],
+                'status' => ['required', 'boolean'],
+            ];
+        }
+        
+        // Para PATCH
+        return [
+            'name' => ['sometimes', 'required', 'string', 'max:255'],
+            'description' => ['sometimes', 'nullable', 'string', 'max:500'],
+            'featured_image' => ['sometimes', 'nullable', 'string'],
+            'status' => ['sometimes', 'required', 'boolean'],
+        ];
+    }
+
+    /**
+     * This method is used to modify the input data before it is validated.
+     * 
+     * @return void
+     */
+    protected function prepareForValidation()
+    {
+        if ($this->has('name')) {
+            $this->merge(['name' => Str::headline($this->input('name'))]);
+        }
+        
+        if ($this->has('description')) {
+            $this->merge([
+                'description' => trim($this->input('description')) !== '' 
+                    ? Str::of($this->input('description'))->trim()->ucfirst()->toString()
+                    : null
+            ]);
+        }
+        
+        if ($this->has('status')) {
+            $this->merge(['status' => (bool) $this->input('status')]);
+        }
+    }
+}
