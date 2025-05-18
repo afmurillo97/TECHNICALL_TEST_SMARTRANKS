fbdd67ce2ebaa911afae0044a8d68f9a3a1a03b7
Andres Murillo <afmurillo97@gmail.com>
2025-05-18 03:49:51 -0500

    Add Swagger configuration and API documentation for SmartRanks
    This commit introduces a new configuration file for Swagger, enabling API documentation for the SmartRanks Technical Test API. It includes routes for accessing the Swagger UI and the generated JSON documentation, along with a new Blade view for rendering the Swagger interface. Additionally, a sample API documentation JSON file is added, detailing endpoints for user authentication and category management, enhancing the overall API usability and developer experience.

---
+++ resources/views/swagger.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>API SmartRanks Documentation</title>
+    @vite(['resources/js/swagger.js'])
+</head>
+<body>
+    <div id="swagger-ui"></div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::view('/api/documentation', 'swagger');
+
+// Ruta para el JSON generado (asegúrate que existe)
+Route::get('/api-docs.json', function () {
+    return response()->file(storage_path('api-docs/api-docs.json'));
+});
